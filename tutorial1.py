c0964b234ffdf2c15ec76098bf81570565115e4f
maury <maureencobbinah@gmail.com>
2025-03-04 18:43:28 +0000

    started my python tutorials

---
+++ tutorial1.py
@@ -0,0 +1,15 @@
+#A BAND NAME GENERATOR
+print ("Hello, Welcome to the band generator")
+
+
+#Asking the user for the city they grew in
+city = input("Where are you from?\n ")
+
+
+#Asking the user the name of their pet
+pet = input("What is the name of your pet?\n ")
+
+
+#Adding the name of the city and the pet to generate the band name
+Band_name = print(city +" "+ pet)
+
