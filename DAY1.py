a5bba488808dffdb38255b67c95c4cda0745e0b8
maury <maureencobbinah@gmail.com>
2025-03-15 02:35:17 +0000

    100 days of coding

---
+++ DAY1.py
@@ -0,0 +1,16 @@
+#Triple quotes alllow you to print a lot of statements in one print command
+print("""
+      Here is an exact bit""")
+
+#python is case-sensitive
+#so apparently in python, when you use "Print" instead of "print", you would incur an error 
+#Print('Hello World')
+
+#DAY1 CHALLENGE 
+print(""" Maureen
+March 13th 2025
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so i can do this from my phone wherever i happen to be. 
+No excuses for not coding from the middle of a field!
+I am feeling excited, challenged and ready""")
