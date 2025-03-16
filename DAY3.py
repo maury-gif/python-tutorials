f8478d8890d9e433ca58129949a3b961887e4f2e
maury <maureencobbinah@gmail.com>
2025-03-16 19:25:31 +0000

    100 days of coding

---
+++ DAY3.py
@@ -0,0 +1,8 @@
+#CONCACTENATION
+#DAY4 challenge
+Food = input("Food: ")
+Plant = input("Plant: ")
+CookingMethod = input("method: ")
+BurnedFood = input("What word describes burned?: ")
+DIYitem = input("Name a DIY item: ")
+print("MENU: ", CookingMethod, Food, "with", BurnedFood, Plant, "on a bed of", DIYitem)
---
+++ DAY4.py
@@ -0,0 +1,13 @@
+#DAY 4 CHALLENGE
+#PROJECT DAY! WRITE YOUR OWN STORY
+print("===\033[33'YOUR ADVENTURE STIMUALTOR'\033[0m===")
+print("""\033[32mYou would be asked a couple of questions then we would make you up an amazing story with YOU as the star!\033[0m """)
+print()
+
+name = input("Your name: ")
+enemy = input("Your enemy's name:  ")
+superpower = input("Your super power: ")
+print()
+print("Our story begins as our hero", name, "yearns to meet to a techie. They finally meet but then...")
+print("Unfortunately a bolt of disappointment striked the mind of the princess", name, "which is very unusual")
+print("\033[31m'A frivolous person!\033[0m(to the princess), says the evil", enemy, "clearly missing the fact that", name, "has the power of\033[36m", superpower, "\033[0mwhich means she'll win quite easily")
